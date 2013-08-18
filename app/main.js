afb7328f1e13eac29cc2194c07213523472ece92
Madalina <madi.mutihac@gmail.com>
2013-08-18 22:54:11 +0200

    initial setup of nodejs to serve statis pages and included emberjs. twitter bootstrap template

--- app/main.js
+++ app/main.js
@@ -1,17 +1,46 @@
+
 // Load the http module to create an http server.
 var http = require('http');
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-
-  response.end("<html><h1 style = 'text-align: center'><code>Welcome to my lair. I ain't got cookies, sorry.</code></h1>\
-  <h2 style='text-align: center'><code>-madalina</code></h2>\
-  </html>");
-});
-
+var fs = require('fs');
+var path = require('path');
 var port = process.env.PORT != undefined ? process.env.PORT : 8080;
-server.listen(port);
 
-// Put a friendly message on the terminal
+
+http.createServer(function (request, response) {
+		var filePath = '.' + request.url;
+		if (filePath == './')
+			filePath = 'app/index.html';
+		
+		var extname = path.extname(filePath);
+		var contentType = 'text/html';
+		switch (extname) {
+			case '.js':
+				contentType = 'text/javascript';
+				break;
+			case '.css':
+				contentType = 'text/css';
+				break;
+		}
+	
+		path.exists(filePath, function(exists) {
+	
+			if (exists) {
+				fs.readFile(filePath, function(error, content) {
+					if (error) {
+						response.writeHead(500);
+						response.end();
+					}
+					else {
+						response.writeHead(200, { 'Content-Type': contentType });
+						response.end(content, 'utf-8');
+					}
+				});
+			}
+			else {
+				response.writeHead(404);
+				response.end();
+			}
+	});
+	
+}).listen(port);
 console.log("Node Server running at " +  port);
