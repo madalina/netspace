71f282ca58547471be11b4952297d2a2b5dc19ae
Madalina <madi.mutihac@gmail.com>
2013-08-22 22:49:32 +0200

    render pdf correctly

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var port = process.env.PORT != undefined ? process.env.PORT : 8080;
-var live = true;
+var live = live;
 
 http.createServer(function (request, response) {
 		var filePath = '.' + request.url;
